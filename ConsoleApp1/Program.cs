e4348b92845496d3d3e0a276055eedba3aca07f1
後藤 祥太 <goto2536@toukei.co.jp>
2021-04-06 23:15:16 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Dao;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sql = "select * from hoge where Id = :Id";
+            var person = new Person();
+            person.Id = 1;
+
+            var result = new DbClient().Select<Person>(sql, person);
+        }
+    }
+}
---
+++ Dao/DbClient.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Dao
+{
+    public class DbClient
+    {
+        //接続情報保持用
+        private DbConnection Connection { get; } = new Oracle.ManagedDataAccess.Client.OracleConnection();
+
+        //コンストラクタ
+        public DbClient()
+        {
+            //ここで接続情報を設定する
+            Connection.ConnectionString = @"Data Source = (DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=192.168.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=hoge))) ; User ID = foo; Password = tiger;";
+            //接続開始
+            Connection.Open();
+        }
+
+        public IEnumerable<T> Select<T>(string sql, T param)
+        {
+            return Connection.Query<T>(sql, param);
+        }
+
+        public void Update<T>(string sql, T param)
+        {
+            using (var trun = Connection.BeginTransaction())
+            {
+                Connection.Execute(sql, param);
+
+                trun.Commit();
+            }
+        }
+    }
+}
